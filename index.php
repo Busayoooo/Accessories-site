754660caa36e205435acaec0f3e98c99446f00f8
Busayoooo <abassalex49@gmail.com>
2024-09-19 18:20:03 +0100

    desktop navbar done and main page started

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php 
+
+include("./templates/header.php");
+
+?>
+
+    <style>
+        .section{
+            border: solid;
+            border-left: 15px;
+            border-color: #ff80ab;
+            border-right: 15px;
+        }
+
+        main div{
+            margin-top: 15px;
+        }
+    </style>
+
+    <main>
+        <div class="row">
+            <div class="col l3 section">
+                <h1 class="center"> Categories </h1>
+            </div>
+            <div class="col l8 push-l1">
+                <div class="slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="/img/blue-bracelet-2.jpeg" alt="blue bracelet" class="responsive-img">
+                        </li>
+                        <li>
+                            <img src="/img/multicolor-bracelet-2.jpg" alt="" class="responsive-img">
+                        </li>
+                        <li>
+                            <img src="/img/pink-bracelet.jpg" alt="" class="responsive-img">
+                        </li>
+                        <li>
+                            <img src="/img/blue-bracelet.webp" alt="" class="responsive-img">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
     
 </body>
 </html>
---
+++ templates/header.php
@@ -0,0 +1,57 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Chic Charms</title>
+</head>
+<style>
+    .brand-logo{
+        margin-top: 8px;
+    }
+
+
+    @media only screen and (min-width: 601px) {
+        nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i {
+            height: 85px;
+        }
+    }
+
+    .nav-icon{
+        margin-top: 10px;
+    }
+
+    .navbar-fixed{
+        margin-bottom: 10px;
+    }
+</style>
+<body class="yellow lighten-4">
+
+    <!-- header -->
+
+    <header>
+        <div class="navbar-fixed hide-on-med-and-down">
+            <nav class="z-depth-2 nav-wrapper pink accent-1">
+                <div class="container">
+                    <div class="row">
+                        <a href="index.html" class="brand-logo"><img width="70px" src="./img/logo.png" alt="" class="responsive-img"></a>
+                    <ul class="right">
+                        <li class="nav-icon">
+                            <a href="" class="  yellow-text text-lighten-4"><i class="large material-icons">account_circle</i></a>
+                        </li>
+                        <li class="nav-icon">
+                            <a href="" class="yellow-text text-lighten-4"><i class="large material-icons">shopping_cart</i></a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="navbar-fixed hide-on-large-only">
+            <nav class="z-depth-2 nav-wrapper pink accent-1">
+                
+            </nav>
+        </div>
+    </header>
+    
