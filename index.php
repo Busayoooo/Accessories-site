ef2e138f8fc0de978be00cfc12e258806b527f80
Busayoooo <abassalex49@gmail.com>
2024-09-22 06:33:00 +0100

    second row completed

--- index.php
+++ index.php
@@ -5,18 +5,31 @@ include("./templates/header.php");
 ?>
 
     <style>
-        main div div.l11{
+        main div div.l10{
             margin-top: 30px;
         }
 
-        main div div.l1{
-            margin-top: 20px;
+        main div div.l2{
+            margin-top: 30px;
+        }
+
+        .bold-text{
+            font-weight: bold;
+        }
+
+        .category{
+            background-color: #ff80ab;
+        }
+
+        .section-text{
+            margin-left: 20px;
         }
     </style>
 
     <main>
+        <!-- first row -->
         <div class="row container">
-            <div class="col l1 pull-l1">
+            <div class="col l2 pink accent-1 category">
                 <h5>Type</h5>
                 <ul>
                     <li>
@@ -60,10 +73,10 @@ include("./templates/header.php");
                     </li>
                 </ul>
             </div>
-            <div class="col l11 push-l1 ">
+            <div class="col l10 push-l1 ">
                 <div class="carousel carousel-slider">
-                    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
-                    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
+                    <a class="carousel-item" href="#one!"><img src="./img/blue-bracelet-2.jpeg"></a>
+                    <a class="carousel-item" href="#two!"><img src="./img/pink-bracelet-2.webp"></a>
                     <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"></a>
                     <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"></a>
                     <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"></a>
@@ -71,7 +84,103 @@ include("./templates/header.php");
                 </div>
             </div>
         </div>
-        
+
+        <!-- Deal of the day -->
+
+        <div class="row center">
+            <h1 class="yellow-text text-lighten-4 section-text pink accent-1">Deals of The Day</h1>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/blue-bracelet-2.jpeg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                        
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/pink-bracelet-2.jpg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/multicolor-bracelet-2.jpg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- row 2  -->
+        <div class="row center">
+            <h1 class="yellow-text text-lighten-4 section-text pink accent-1">Deals of The Day</h1>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/blue-bracelet-2.jpeg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                        
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/pink-bracelet-2.jpg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col l4">
+                <div class="card hoverable">
+                    <div class="card-image">
+                        <img src="./img/multicolor-bracelet-2.jpg" alt="" class="responsive-img">
+                    </div>
+                    <div class="card-content">
+                    <p>Marble design teal bracelet without charms and with </p>
+                        <h6 class="Black-text bold-text left">N 1500</h6> <br>
+                    </div>
+                    <div class="card-action">
+                        <a class="pink-text text-accent-1 bold-text" href="">Order Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
     
     <script src="js/jquery.js"></script>
